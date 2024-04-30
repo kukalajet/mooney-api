2de387d9f5752519f5488c2ee4730d99154b67bb
jetonatcrispy <161860947+jetonatcrispy@users.noreply.github.com>
2024-04-30 22:51:23 +0200

    Add firebase-service-key.ts to the repository

---
+++ config/firebase-service-key.ts
@@ -0,0 +1,17 @@
+const firebaseServiceKey = {
+  type: "service_account",
+  project_id: "kuleta-app",
+  private_key_id: "455cedab7eca08af434aab2c59ef00b4ce48426b",
+  private_key:
+    "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCpx5iAytWBLhZQ\niSx6ruVTW5oHtPDraRTg7/gNMDOrCvHPEIalUtpx6pZ9LaIR2XOLdN0Q84ZYf4Cm\nVsx3sHbPOAMZenj9VLtGgAUDQBXVOWHnXC3/0MoL/nnZ91gzbuGcwH7U/t9cRG1T\nmkLXzGqymR/ycj5pniLgxrfa2shBg0EEtFk+WHnSms/NVcqZWi115/0CG4Ch81Th\nNcVCaYSLnf6/LGA/ZGRSQdfISYqn8gF1VyW1TUy8aMii7zorQHwSz6dVoopg7uxN\nNYu1Kz8QSdFVIw2uOMOs5ELPpOqhjXvsFSCtHBh8Nomci8OB66nE4EdcuUVw99t9\nmfKsEDSfAgMBAAECggEANIqF2fmreCZElpjU8ISWiKT09oSa+CdVZEUvLRmpK7of\n2hZBrQEGhDF2b/fCy6ZRWSuJzFioz1NqA1AiR7oPEpLBds2NisyBhlBQ/NAE0a9H\n6pr+y0eYd3zBxeADZite261AsvbLNdkhQjmfjPwAWxQDLHsJcJMVNR2aQTrqtUcB\nbW9UaA5vGPLlN16ShXV6L0ILUG4+wfXuutfbqDvlC4yq6ketbMVPkOjj8Dqilffv\nfatDqf1ylV6BUt5136V6nXwGtFWIkUS8QLilXLni/e1DehnRvqY+Ep5Jwk9JYuOc\nHOuGdZAggA22A2dW1Co0Cn47SXrXTatvWdkCIryLcQKBgQDfSASxDjS5tY5z/QEu\nxuEbaMXz5fFsIGJvh853tJrGM6RdpdgTr+OMI8gSk2wbxWAEKeru4mAYtJjkb+EH\nRX94ZZhkLhjyZ1PJXtXgobhH3hG83fniHV+CAhpkTeW7Gr1JBSQsGZvStxOM45wx\nNIDCgewidySffY+rApUlsJFOTwKBgQDCqJAaZxluA0+41K57KvMBvx++ZHOnQdVZ\nywwzga7k0tT8sZDwxYSDaDkyRLFFGPQPKQRt12zO0HSnmeYRv/bry62YKU7TRV4m\nvl9J3duYgxSzY4XlVePOVQ8y7H5N6aVwzTvtYvrNmMdy+st0v0VMfx1YohMjs2PI\nrve/2iHwsQKBgBvtQRD4KuYTbEbbnTtBisRH3YJ8/y47+rPic5OLVlR1/xfrd1Q3\nCqnri8mvALWUgUbkC0tnqwdMB1eDGs0zBxeC9YWKMnqXK+ai4vNeptLuVp+dz/RV\nIi37n1tuVs6JMKWia/zMEy5O0bYsfcu1umkZoPRKnJ0KMl78SrVVGUeHAoGAAp3j\negdixyDSRIjPY6Z7J83a7mNJTrlK1e8t1c9re3X+GPuGGEZ/dojDJBLaUzR3q3CA\nt71KoCkbOFmQRzLObfMJ/V6oyZxIHLs4arTJy+4PLC4tUZzq+A4aqzOUqxV1/OM1\naAaJmpPwMnJ+o64ij1myGkvrd/JkaAE6IWEcTbECgYEAkUJFxGSnxnWuiugtKxJ0\n0SCOpi8nKu5JAfXdO7g4v4hZ088nyH4WZr71BX2MHBxLQDr7DME1wUKBerEDTSz4\nrbNp1tEEPBIg1wlpH/Aw0aJo1M5j/4NE35Yj8xzT5YDXZUxeWFu2+xNg9HqKMdFA\nyFipjha36c23lBUcbdtSkuQ=\n-----END PRIVATE KEY-----\n",
+  client_email: "firebase-adminsdk-ouhjx@kuleta-app.iam.gserviceaccount.com",
+  client_id: "102350950446865259370",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url:
+    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-ouhjx%40kuleta-app.iam.gserviceaccount.com",
+  universe_domain: "googleapis.com",
+} as const;
+
+export { firebaseServiceKey };
